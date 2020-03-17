883b93e8d20fa0f1e1ca26af949af9d0143e9933
parkgeonhu <parkgeonhu@gmail.com>
2020-03-17 23:57:59 +0900

    Add getting location from image

--- googlemaps/googlemaps_myplace_title.py
+++ googlemaps/googlemaps_myplace_title.py
@@ -7,9 +7,14 @@ import time
 from selenium.webdriver.common.keys import Keys
 
 
+##sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+##from credential import config
+
+SCROLL_PAUSE=3
+
 
 path = "Webdriver 경로를 입력합니다."
-driver = webdriver.Chrome(r'C:\Users\myhome\Desktop\selenium\chromedriver.exe')
+driver = webdriver.Chrome('../util/chromedriver.exe')
 driver.get("https://www.google.com/maps/@/data=!3m1!4b1!4m3!11m2!2scjKYe9Nzb5duDEo4Xrctbk0KuBJyOw!3e3?shorturl=1")
 ##search_box = driver.find_element_by_name("q")
 
@@ -20,11 +25,20 @@ element = driver.find_element_by_class_name("section-scrollbox")
 
 print(element.get_attribute('innerHTML'))
 
-for count in range(10):
-    driver.execute_script("arguments[0].scroll({top:10000});", element)
-    time.sleep(1)
+##for count in range(10):
+##    driver.execute_script("arguments[0].scroll({top:10000});", element)
+##    time.sleep(1)
 
+last_height = driver.execute_script("return arguments[0].scrollHeight;", element)
 
+while True:
+    driver.execute_script("arguments[0].scrollTo(0, arguments[0].scrollHeight);", element)
+    time.sleep(SCROLL_PAUSE)
+
+    new_height = driver.execute_script("return arguments[0].scrollHeight;", element)
+    if new_height == last_height:
+        break
+    last_height = new_height
 
 time.sleep(5)
 
