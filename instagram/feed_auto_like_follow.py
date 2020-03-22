7f7a0ce683ad43b15b4db4bebae145581a76fd2a
parkgeonhu <parkgeonhu@gmail.com>
2020-03-22 22:51:05 +0900

    Fix auto like, follow logic to prevent noSuchElement error handling

--- instagram/feed_auto_like_follow.py
+++ instagram/feed_auto_like_follow.py
@@ -45,10 +45,45 @@ def get_driver():
 
 def followLiker(id_list):
     driver=get_driver()
-    id_list=['sanha_1.17']
-    driver.get("https://www.instagram.com/"+id_list[0])
+    id_list=['geon__after']
 
-    time.sleep(2)
+    
+
+    #id 리스트에서 id 가져와서 중간 for문에 넘겨줌
+    for id in id_list:
+        driver.get("https://www.instagram.com/"+id)
+        time.sleep(2)
+
+        
+        #팔로우 찍는 버튼
+        driver.find_element_by_class_name("vBF20").click()
+
+        post_count= WebDriverWait(driver, 5).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, '#react-root > section > main > div > header > section > ul > li:nth-child(1) > span > span'))
+            ).text.replace(",","")
+
+        post_count=int(post_count)
+        
+        first_post_xpath = '//*[@id="react-root"]/section/main/div/div[2]/article/div[1]/div/div[1]/div[1]/a/div[1]/div[2]'
+        first_post = driver.find_element_by_class_name("eLAPa").click()
+        
+        driver.implicitly_wait(5)
+
+        #게시물 좋아요 찍어주는 로직
+        for i in range(post_count):
+            if i>4:
+                break
+            post_like_btn = driver.find_element_by_class_name("fr66n")
+            driver.find_element_by_class_name("fr66n").click()
+            time.sleep(1)
+            ##수정 봐야 할 로직 range variable이
+            if i==post_count-1:
+                break
+            driver.find_element_by_class_name("coreSpriteRightPaginationArrow").click()
+            time.sleep(1)
+
+
+
 
 ##    follower_count= WebDriverWait(driver, 5).until(
 ##        EC.presence_of_element_located((By.CSS_SELECTOR, '#react-root > section > main > div > header > section > ul > li:nth-child(2) a > span'))
@@ -60,21 +95,10 @@ def followLiker(id_list):
 ##        EC.presence_of_element_located((By.CSS_SELECTOR, '#react-root > section > main > div > header > section > ul > li:nth-child(1) a > span'))
 ##    ).text.replace(",","")
 
-##    driver.find_element_by_class_name("vBF20").click()
 
 
-    first_post_xpath = '//*[@id="react-root"]/section/main/div/div[2]/article/div[1]/div/div[1]/div[1]/a/div[1]/div[2]'
-    first_post = driver.find_element_by_class_name("eLAPa").click()
 
-
-    driver.implicitly_wait(5)
     
-##    for i in range(16):
-##        post_like_btn = driver.find_element_by_class_name("fr66n")
-##        driver.find_element_by_class_name("fr66n").click()
-##        time.sleep(1)
-##        ##수정 봐야 할 로직 range variable이 
-##        driver.find_element_by_class_name("coreSpriteRightPaginationArrow").click()
 
 if __name__ == "__main__":
     followLiker([])
